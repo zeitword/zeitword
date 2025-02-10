ff5ce5ad2e8d2a89a3cc452c5266a8a823c2c713
Tom Härter <tomuhaerter@gmail.com>
2025-02-10 12:29:57 +0100

    site page

--- app/pages/sites/index.vue
+++ app/pages/sites/index.vue
@@ -15,14 +15,16 @@ async function createSite() {
 <template>
   <div class="p-4">
     <div>Sites</div>
-    <form action="" @submit.prevent="createSite" class="flex max-w-sm flex-col gap-2">
+    <form @submit.prevent="createSite" class="mb-4 flex max-w-sm flex-col gap-2">
       <DInput type="text" v-model="name" placeholder="Name of the site" />
       <DButton type="submit">Create Site</DButton>
     </form>
-    <div class="p-4">
-      <ul class="ml-4 list-disc">
-        <li v-for="site in data" :key="site.id">{{ site.name }} - {{ site.id }}</li>
-      </ul>
+    <div class="">
+      <div class="grid gap-4" style="grid-template-columns: repeat(auto-fill, 150px)">
+        <NuxtLink :to="`/sites/${site.id}`" v-for="site in data" :key="site.id" class="aspect-[3/4] w-full rounded bg-black p-4 text-white">
+          {{ site.name }}
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
---
+++ server/api/sites/[id]/index.get.ts
@@ -0,0 +1,15 @@
+import { sites } from "~~/server/database/schema"
+
+export default defineEventHandler(async (event) => {
+  const { secure } = await requireUserSession(event)
+  if (!secure) throw createError({ statusCode: 401, statusMessage: "Unauthorized" })
+
+  const id = getRouterParam(event, "id")
+  if (!id) throw createError({ statusCode: 400, statusMessage: "Invalid ID" })
+
+  const result = await useDrizzle()
+    .select()
+    .from(sites)
+    .where(and(eq(sites.id, id), eq(sites.organisationId, secure.organisationId)))
+  return result
+})
